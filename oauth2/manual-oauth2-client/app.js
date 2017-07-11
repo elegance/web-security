96f96b5e5b8274345ef2e7b0e3d371632d4f0c22
elegance <webdevelop@sohu.com>
2017-07-11 17:02:26 +0800

    更新适用 nunjucks 实现模板渲染

--- oauth2/manual-oauth2-client/app.js
+++ oauth2/manual-oauth2-client/app.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var nunjucks = require('nunjucks');
-var path = require("path");
+var path = require('path');
+var fetch = require('node-fetch');
 
 var app = express();
-nunjucks.configure(path.join(__dirname), 'views', { // 设置模板文件目录
+nunjucks.configure(path.join(__dirname, 'views'), { // 设置模板文件目录
     autoescape: true,
-    express: app
+    express: app,
+    watch: true
 });
 
 app.use(express.static('public'));
@@ -16,37 +18,29 @@ const client_id = '2882303761517520186';
 const client_secret = '63ae5dc5-8fa4-4366-8eda-29e6da172d92'; // 注意：secret 只可保存在后台，否则secret泄露导致安全问题(另外 oauth server 后台也可以限定 access_token申请的白名单IP)
 
 app.all('/account/login', (req, res, next) => {
-    let query = req.query;
-    let oauthProvide = query.oauth_provide;
+    let {code, oauth_provide} = req.query;
 
-    if (oauthProvide === 'custom') { //自定义的oauth
-        // 根据 code 取 access_token 等信息
-        let authorizeCode = query.code;
-        res.send(`授权码：${authorizeCode}，TODO: 下一步，后台根据授权码与oauth服务商申请access_token`);
-    } else {
-        res.send('不支持的oauth_provide');
-    }
-    next();
+    res.render('showAuthorizeCode.html', {
+        code,
+        oauth_provide
+    })
 });
 
-app.all('/applyAccesToken', (req, res, next) => {
+app.all('/applyAccessToken', (req, res, next) => {
     let authorizeCode = req.query.authorizeCode;
+    console.log(`code=${authorizeCode}&client_id=${client_id}&client_secret=${client_secret}`);
+
     fetch(oauth_server + '/oauth2/access_token', {
         headers: {
+            method: 'POST',
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
         },
         body: `code=${authorizeCode}&client_id=${client_id}&client_secret=${client_secret}`
     }).then(oResp => {
         oResp.json().then(ret => {
-            if (ret.code === '0000') {
-                let accessToken = ret.data;
-                res.send(`获取到的Token: ${accessToken}`);
-            } else {
-                res.send(JSON.stringify(ret));
-            }
-            
+            console.log(ret);
+            res.send(ret);
         });
-        next();
     });
 });
 
